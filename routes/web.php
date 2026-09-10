45edc36566ade8e0fcd7caea82bc6a2b2672459a
shhrlhakxm <shahrulhakim63@gmail.com>
2026-09-10 19:58:40 +0800

    feat(routes): add new route for stock-movement

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ItemController;
+use App\Http\Controllers\StockMovementController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -21,12 +22,22 @@ Route::middleware(['auth', 'admin'])->group(function () {
 });
 
 Route::middleware('auth')->group(function () {
+
+    // Profile routes
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     //Item routes
     Route::resource('items', ItemController::class);
+
+    // Additional routes for items
+    Route::get('items/{item}/stock-movements/create/{type}', [StockMovementController::class, 'create'])
+        ->whereIn('type', ['in', 'out', 'adjustment'])
+        ->name('stock-movements.create');
+
+    Route::post('items/{item}/stock-movements', [StockMovementController::class, 'store'])
+        ->name('stock-movements.store');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
