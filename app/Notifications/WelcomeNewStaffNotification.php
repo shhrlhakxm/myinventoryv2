d64657352fb232de69aae44259010ce3d4255723
shhrlhakxm <shahrulhakim63@gmail.com>
2026-09-11 16:04:43 +0800

    feat(notifications): add new custom email set password for new user

---
+++ app/Notifications/WelcomeNewStaffNotification.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\Facades\Url;
+use Illuminate\Support\Facades\Lang;
+
+class WelcomeNewStaffNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(public string $token)
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+
+        $url = url(route('password.reset', [
+            'token' => $this->token,
+            'email' => $notifiable->getEmailForPasswordReset(),
+        ], false));
+
+        return (new MailMessage)
+            ->subject('Welcome! — Please Set Your Password')
+            ->greeting("Hi {$notifiable->name},")
+            ->line('Your account has been created in the MyInventory system.')
+            ->action('Set Password', $url)
+            ->line('This link will expire in ' . config('auth.passwords.users.expire') . ' minutes.');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
